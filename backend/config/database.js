236a2cc6ccd61a6d04551a141ba169640f2955f9
Sandeepan Kalita <77795912+redhawk2002@users.noreply.github.com>
2023-02-12 13:15:08 +0530

    Create database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI,{ useNewUrlParser: true })
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
