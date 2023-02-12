9b41e833c7c9fdb4b3698840bf4787751c02ccbf
Sandeepan Kalita <77795912+redhawk2002@users.noreply.github.com>
2023-02-12 13:54:47 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,7 @@ require("dotenv").config({ path: "./config.env" });
 mongoose.set("strictQuery", false);
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URI,{ useNewUrlParser: true })
+    .connect("mongodb+srv://Sandeepan:12345qaz@cluster0.fvtc2wm.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true })
     .then((con) => console.log(`Database Connected: ${con.connection.host}`))
     .catch((err) => console.log(err));
 };
